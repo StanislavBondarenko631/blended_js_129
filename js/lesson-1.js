d944f8a08f0c8b5ba9788381ca9963cac6f122be
StanislavBondarenko631 <skyritm201287@gmail.com>
2026-05-25 13:27:58 +0300

    practice js module1,2

--- js/lesson-1.js
+++ js/lesson-1.js
@@ -162,3 +162,24 @@
 //   }
 // }
 // console.log(isAdult());
+
+// Напишіть функцію fizzBuzz(num), яка приймає число і перевіряє кожне число від 1 до num:
+// Якщо число ділитися  без остачі на 3 - виводить в консоль 'fizz',
+// якщо ділиться  без остачі на 5 - виводить в консоль 'buzz',
+// якщо ділиться  без остачі і на 3, і на 5 - виводить в консоль 'fizzbuzz'.
+
+// function fizzBuzz(num) {
+//   let result = 1;
+//   for (let i = 1; i <= num; i += 1) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//       console.log("fizzbuzz");
+//     } else if (i % 3 === 0) {
+//       console.log("fizz");
+//     } else if (i % 5 === 0) {
+//       console.log("buzz");
+//     } else {
+//       console.log(i);
+//     }
+//   }
+// }
+// fizzBuzz(15);
